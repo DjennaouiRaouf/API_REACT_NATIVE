48fabbdcb622a57361ee94ec32faf8b058d97c8a
rdjennaoui <djennaoui.raouf@gmail.com>
2023-06-23 22:57:57 +0200

    Initial commit

--- API/views.py
+++ API/views.py
@@ -1,8 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import authenticate
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login,logout
 
 class LoginView(APIView):
     permission_classes = []
@@ -11,6 +10,7 @@ class LoginView(APIView):
         password = request.data.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
+            login(request,user)
             return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
         else:
             return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
